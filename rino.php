0d049b9844c86de7e86a09fd92d892108c62c0b7
Rino Adif Ismawan <48473709+RinoAdifIsmawan@users.noreply.github.com>
2019-03-22 21:40:13 +0700

    Add files via upload

---
+++ rino.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Tugas 1</title>
+</head>
+<body>
+	
+	<h1 style="text-align:center">Hello World</h1>
+	&nbsp;
+	<p><b>NAMA  : RINO ADIF ISAMAWAN</b></p>
+	<p><b>NIM   : 16.01.53.0116</b></p>
+	
+</body>
+</html>
