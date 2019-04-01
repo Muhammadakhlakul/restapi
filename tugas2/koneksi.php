208f0a63b5cab818fb6c313910d6e71c78da0e20
Rino Adif Ismawan <48473709+RinoAdifIsmawan@users.noreply.github.com>
2019-04-01 22:10:20 +0700

    Update koneksi.php

--- tugas2/koneksi.php
+++ tugas2/koneksi.php
@@ -1,1 +1,9 @@
-
+<?php 
+$koneksi = mysqli_connect("localhost","id9012695_rino_ismawan","rino1","id9012695_akademik1");
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}
+ 
+?>
